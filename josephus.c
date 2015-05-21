a505326e768f85a1cc749c5102d97d2e33ecbf4a
liuwei <liuwei159528@163.com>
2015-05-21 20:32:00 +0800

    Josephus problem

---
+++ josephus.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define NUM 10
+typedef struct node
+{
+  int data;
+  struct node *next;
+}node;
+
+node *create(int n)
+{
+  node *p=NULL, *head;
+  head=(node *)malloc(sizeof(node));
+  p=head;
+  node *s;
+  int i=1;
+
+ if(0!=n)
+ { 
+   while(i<=n)
+   { 
+      s=(node *)malloc(sizeof(node));
+      s->data=i++;
+      p->next=s;
+      p=s;
+   }
+   s->next=head->next;
+ }
+  free(head);
+  return s->next;
+}
+
+int main()
+{ 
+  int n=NUM;
+  int m=3;
+  int i;
+  node *p=create(n);
+  node *temp;
+
+  m=m%n;
+  while(p!=p->next)
+  {
+    for(i=1;i<m-1;i++)
+    {
+      p=p->next;
+    }
+    printf("%d ->",p->next->data);
+    temp=p->next;
+    p->next=temp->next;
+    free(temp);
+    p=p->next;
+  }
+  printf("%d\n",p->data);
+  return 0;
+}
